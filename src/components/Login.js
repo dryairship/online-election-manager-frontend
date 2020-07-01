efe1b815ed97c61eb268dfd5dfab22d5680266e7
Priydarshi Singh <dryairship@gmail.com>
2020-07-01 21:52:24 +0530

    Complete login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Alert from '@material-ui/lab/Alert';
+import sjcl from 'sjcl';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -29,17 +31,53 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
   const classes = useStyles();
 
+  const [lStatus, setLStatus] = useState({});
+
+  const login = () => {
+    let rollVal = document.getElementById("l-roll").value;
+    let passwordVal = document.getElementById("l-password").value;
+    let passwordHash = sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(passwordVal));
+
+    let formData = new FormData();
+    formData.append('roll', rollVal);
+    formData.append('pass', passwordHash);
+
+    fetch("/users/login", {
+      body: formData,
+      method: "POST"
+    }).then(
+      response => {
+        let code = response.status;
+        response.text().then(text => 
+          setLStatus({
+            display: true,
+            severity: code === 200 ? "success" : "error",
+            message: text,
+          })
+        );
+      },
+      _ => setLStatus({
+        display: true,
+        severity: "error",
+        message: "Error while making a request. Please check your internet connection."
+      }),
+    );
+  }
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <form className={classes.form} noValidate>
+          {lStatus.display && // Only display if lStatus.display is true
+            <Alert severity={lStatus.severity}>{lStatus.message}</Alert>
+          }
           <TextField
             variant="outlined"
             margin="normal"
             required
             fullWidth
-            id="roll"
+            id="l-roll"
             label="Roll Number"
             name="roll"
             autoComplete="roll"
@@ -53,14 +91,15 @@ export default function Login() {
             name="password"
             label="Password"
             type="password"
-            id="password"
+            id="l-password"
             autoComplete="current-password"
           />
           <Grid container justify="center">
             <Button
-              type="submit"
+              type="button"
               variant="contained"
               color="primary"
+              onClick={login}
               className={classes.submit}
             >
               Sign In

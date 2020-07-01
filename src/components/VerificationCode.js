36d955c4c57037c6066a1491b072487a6aeff5f0
Priydarshi Singh <dryairship@gmail.com>
2020-07-01 18:51:42 +0530

    Implement CAPTCHA rendering

--- src/components/VerificationCode.js
+++ src/components/VerificationCode.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Captcha from './Captcha';
+import Alert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -33,14 +33,31 @@ const useStyles = makeStyles((theme) => ({
 export default function VerificationCode() {
   const classes = useStyles();
 
+  const [captcha, setCaptcha] = useState({});
+  const [vcError, setError] = useState(null);
+
+  useEffect(() => {
+    if(!captcha.id) {
+      fetch("/users/captcha")
+      .then(res => res.json())
+      .then(
+          result => setCaptcha(result),
+          _ => setError('CAPTCHA service is not working'),
+        );
+    }
+  });
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <form className={classes.form} noValidate>
-
-          <Captcha/>
-
+          {vcError && // Only display if vcError is not null
+            <Alert severity="error">{vcError}</Alert>
+          }
+          <Grid container justify="center">
+            <img src={captcha.value} alt="CAPTCHA" />
+          </Grid>
           <TextField
             variant="outlined"
             margin="normal"

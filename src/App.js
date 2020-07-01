ec2925197b3a45707f38ba52b4dc749e2aa04a82
Priydarshi Singh <dryairship@gmail.com>
2020-07-01 17:34:08 +0530

    Add login component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from '@material-ui/core/Box';
+import Login from './components/Login';
+import Copyright from './components/Copyright';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      style={{
+        position: 'absolute', left: '50%', top: '50%',
+        transform: 'translate(-50%, -50%)'
+      }}
+    >
+      <Login/>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
     </div>
   );
 }

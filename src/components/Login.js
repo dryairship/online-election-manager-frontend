6fe128afd0cf04ff29db0d7ec4a552849eae0256
Sparsh Sihotiya <sparshs@iitk.ac.in>
2022-01-10 18:47:26 +0530

    added admin-pass field in login

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,119 +29,91 @@ export default function Login(props) {
   const [captcha, setCaptcha] = useState({});
   const [lStatus, setLStatus] = useState({});
 
-  const sha = text => sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(text));
+  const sha = (text) => sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(text));
 
   useEffect(() => {
-    if(!captcha.id) {
+    if (!captcha.id) {
       captcha.id = true;
-      fetch("/api/users/captcha")
-      .then(res => res.json())
-      .then(
-        result => setCaptcha(result),
-        _ => setLStatus({
-          display: true,
-          severity: "error",
-          message: "CAPTCHA service is not working",
-        }),
-      );
+      fetch('/api/users/captcha')
+        .then((res) => res.json())
+        .then(
+          (result) => setCaptcha(result),
+          (_) =>
+            setLStatus({
+              display: true,
+              severity: 'error',
+              message: 'CAPTCHA service is not working',
+            })
+        );
     }
   });
 
   const login = () => {
-    let rollVal = document.getElementById("l-roll").value;
-    let passwordVal = document.getElementById("l-password").value;
-    let captchaValue = document.getElementById("l-captcha").value;
+    let rollVal = document.getElementById('l-roll').value;
+    let passwordVal = document.getElementById('l-password').value;
+    let adminpasswordVal = document.getElementById('l-adminpassword').value;
+    let captchaValue = document.getElementById('l-captcha').value;
     let passwordHash = sha(sha(sha(passwordVal)));
+    let adminpasswordHash = sha(sha(sha(adminpasswordVal)));
 
     let formData = new FormData();
     formData.append('roll', rollVal);
     formData.append('pass', passwordHash);
     formData.append('captchaId', captcha.id);
     formData.append('captchaValue', captchaValue);
+    formData.append('adminpass', adminpasswordHash);
 
-    fetch("/api/users/login", {
+    fetch('/api/users/login', {
       body: formData,
-      method: "POST"
+      method: 'POST',
     }).then(
-      response => {
+      (response) => {
         let code = response.status;
-        if(code === 200) {
-          response.json().then(userData =>{
+        if (code === 200) {
+          response.json().then((userData) => {
             props.onLogin({
               roll: rollVal,
               password: passwordVal,
               data: userData,
-            })
+            });
           });
         } else {
-          response.text().then(text => 
+          response.text().then((text) =>
             setLStatus({
               display: true,
-              severity: "error",
+              severity: 'error',
               message: text,
             })
           );
         }
       },
-      _ => setLStatus({
-        display: true,
-        severity: "error",
-        message: "Error while making a request. Please check your internet connection."
-      }),
+      (_) =>
+        setLStatus({
+          display: true,
+          severity: 'error',
+          message: 'Error while making a request. Please check your internet connection.',
+        })
     );
     setCaptcha({});
-  }
+  };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component='main' maxWidth='xs'>
       <CssBaseline />
       <div className={classes.paper}>
-        {lStatus.display && // Only display if lStatus.display is true
+        {lStatus.display && ( // Only display if lStatus.display is true
           <Alert severity={lStatus.severity}>{lStatus.message}</Alert>
-        }
+        )}
         <form className={classes.form} noValidate>
-          <Grid container justify="center">
-            <img src={captcha.value} alt="CAPTCHA" />
+          <Grid container justify='center'>
+            <img src={captcha.value} alt='CAPTCHA' />
           </Grid>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="l-captcha"
-            label="CAPTCHA"
-            name="captcha"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="l-roll"
-            label="Roll Number"
-            name="roll"
-            autoComplete="roll"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="l-password"
-            autoComplete="current-password"
-          />
-          <Grid container justify="center">
-            <Button
-              type="button"
-              variant="contained"
-              color="primary"
-              onClick={login}
-              className={classes.submit}
-            >
+          <TextField variant='outlined' margin='normal' required fullWidth id='l-captcha' label='CAPTCHA' name='captcha' autoFocus />
+          <TextField variant='outlined' margin='normal' required fullWidth id='l-roll' label='Roll Number' name='roll' autoComplete='roll' />
+          <TextField variant='outlined' margin='normal' required fullWidth name='password' label='Password' type='password' id='l-password' autoComplete='current-password' />
+          <TextField variant='outlined' margin='normal' required fullWidth name='adminpassword' label='Admin Password' type='password' id='l-adminpassword' />
+          <Grid container justify='center'>
+            <Button type='button' variant='contained' color='primary' onClick={login} className={classes.submit}>
               Sign In
             </Button>
           </Grid>

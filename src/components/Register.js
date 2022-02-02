59b2a5945c63a80a5905fdd8904a2ad167567f81
Sparsh Sihotiya <sparshs@iitk.ac.in>
2022-02-03 03:08:50 +0530

    added local storage check, removed admin password

--- src/components/Register.js
+++ src/components/Register.js
@@ -9,175 +9,126 @@ import Alert from '@material-ui/lab/Alert';
 import sjcl from 'sjcl-all';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(2),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+	paper: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(2),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 }));
 
 export default function Register() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const [captcha, setCaptcha] = useState({});
-  const [rStatus, setRStatus] = useState({});
+	const [captcha, setCaptcha] = useState({});
+	const [rStatus, setRStatus] = useState({});
 
-  const sha = text => sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(text));
-  const sha1 = text => sjcl.codec.hex.fromBits(sjcl.hash.sha1.hash(text));
+	const sha = (text) => sjcl.codec.hex.fromBits(sjcl.hash.sha256.hash(text));
+	const sha1 = (text) => sjcl.codec.hex.fromBits(sjcl.hash.sha1.hash(text));
 
-  useEffect(() => {
-    if(!captcha.id) {
-      captcha.id = true;
-      fetch("/api/users/captcha")
-      .then(res => res.json())
-      .then(
-        result => setCaptcha(result),
-        _ => setRStatus({
-          display: true,
-          severity: "error",
-          message: "CAPTCHA service is not working",
-        }),
-      );
-    }
-  });
+	useEffect(() => {
+		if (!captcha.id) {
+			captcha.id = true;
+			fetch('/api/users/captcha')
+				.then((res) => res.json())
+				.then(
+					(result) => setCaptcha(result),
+					(_) =>
+						setRStatus({
+							display: true,
+							severity: 'error',
+							message: 'CAPTCHA service is not working',
+						})
+				);
+		}
+	});
 
-  const registerUser = () => {
-    let rollVal = document.getElementById("r-roll").value;
-    let passwordVal = document.getElementById("r-password").value;
-    let confirmPasswordVal = document.getElementById("r-confirm-password").value;
-    let verificationCodeVal = document.getElementById("r-verification-code").value;
-    let captchaValue = document.getElementById("r-captcha").value;
+	const registerUser = () => {
+		let rollVal = document.getElementById('r-roll').value;
+		let passwordVal = document.getElementById('r-password').value;
+		let confirmPasswordVal = document.getElementById('r-confirm-password').value;
+		let verificationCodeVal = document.getElementById('r-verification-code').value;
+		let captchaValue = document.getElementById('r-captcha').value;
 
-    if(passwordVal !== confirmPasswordVal) {
-      setRStatus({
-        display: true,
-        severity: "error",
-        message: "The passwords do not match."
-      });
-      return;
-    }
+		if (passwordVal !== confirmPasswordVal) {
+			setRStatus({
+				display: true,
+				severity: 'error',
+				message: 'The passwords do not match.',
+			});
+			return;
+		}
 
-    let passwordHash = sha(sha(sha(passwordVal)));
-    let sha1code = sha1(sha1(sha1(verificationCodeVal)));
-    let sha256code = sha(sha(sha(verificationCodeVal)));
-    let passXac = sjcl.encrypt(passwordVal, verificationCodeVal);
+		let passwordHash = sha(sha(sha(passwordVal)));
+		let sha1code = sha1(sha1(sha1(verificationCodeVal)));
+		let sha256code = sha(sha(sha(verificationCodeVal)));
+		let passXac = sjcl.encrypt(passwordVal, verificationCodeVal);
 
-    let formData = new FormData();
-    formData.append('roll', rollVal);
-    formData.append('pass', passwordHash);
-    formData.append('auth', sha256code);
-    formData.append('sha1', sha1code);
-    formData.append('passXac', passXac);
-    formData.append('captchaId', captcha.id);
-    formData.append('captchaValue', captchaValue);
+		let formData = new FormData();
+		formData.append('roll', rollVal);
+		formData.append('pass', passwordHash);
+		formData.append('auth', sha256code);
+		formData.append('sha1', sha1code);
+		formData.append('passXac', passXac);
+		formData.append('captchaId', captcha.id);
+		formData.append('captchaValue', captchaValue);
 
-    fetch("/api/users/register", {
-      body: formData,
-      method: "POST"
-    }).then(
-      response => {
-        let code = response.status;
-        if(code===202) {
-          document.getElementById("registration-form").reset();
-        }
-        response.text().then(text => 
-          setRStatus({
-            display: true,
-            severity: code === 202 ? "success" : "error",
-            message: text,
-          })
-        );
-      },
-      _ => setRStatus({
-        display: true,
-        severity: "error",
-        message: "Error while making a request. Please check your internet connection."
-      }),
-    );
-    setCaptcha({});
-  }
+		fetch('/api/users/register', {
+			body: formData,
+			method: 'POST',
+		}).then(
+			(response) => {
+				let code = response.status;
+				if (code === 202) {
+					document.getElementById('registration-form').reset();
+				}
+				response.text().then((text) =>
+					setRStatus({
+						display: true,
+						severity: code === 202 ? 'success' : 'error',
+						message: text,
+					})
+				);
+			},
+			(_) =>
+				setRStatus({
+					display: true,
+					severity: 'error',
+					message: 'Error while making a request. Please check your internet connection.',
+				})
+		);
+		setCaptcha({});
+	};
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        {rStatus.display && // Only display if rStatus.display is true
-          <Alert severity={rStatus.severity}>{rStatus.message}</Alert>
-        }
-        <form className={classes.form} noValidate id="registration-form">
-          <Grid container justify="center">
-            <img src={captcha.value} alt="CAPTCHA" />
-          </Grid>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="r-captcha"
-            label="CAPTCHA"
-            name="captcha"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="r-roll"
-            label="Roll Number"
-            name="roll"
-            autoComplete="roll"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="r-password"
-            autoComplete="current-password"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="confirm-password"
-            label="Confirm Password"
-            type="password"
-            id="r-confirm-password"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="r-verification-code"
-            label="Verification Code"
-            name="verification-code"
-          />
-          <Grid container justify="center">
-            <Button
-              type="button"
-              variant="contained"
-              color="primary"
-              onClick={registerUser}
-              className={classes.submit}
-            >
-              Register
-            </Button>
-          </Grid>
-        </form>
-      </div>
-    </Container>
-  );
+	return (
+		<Container component='main' maxWidth='xs'>
+			<CssBaseline />
+			<div className={classes.paper}>
+				{rStatus.display && ( // Only display if rStatus.display is true
+					<Alert severity={rStatus.severity}>{rStatus.message}</Alert>
+				)}
+				<form className={classes.form} noValidate id='registration-form'>
+					<Grid container justify='center'>
+						<img src={captcha.value} alt='CAPTCHA' />
+					</Grid>
+					<TextField variant='outlined' margin='normal' required fullWidth id='r-captcha' label='CAPTCHA' name='captcha' autoFocus />
+					<TextField variant='outlined' margin='normal' required fullWidth id='r-roll' label='Roll Number' name='roll' autoComplete='roll' />
+					<TextField variant='outlined' margin='normal' required fullWidth name='password' label='Password' type='password' id='r-password' autoComplete='current-password' />
+					<TextField variant='outlined' margin='normal' required fullWidth name='confirm-password' label='Confirm Password' type='password' id='r-confirm-password' />
+					<TextField variant='outlined' margin='normal' required fullWidth id='r-verification-code' label='Verification Code' name='verification-code' />
+					<Grid container justify='center'>
+						<Button type='button' variant='contained' color='primary' onClick={registerUser} className={classes.submit}>
+							Register
+						</Button>
+					</Grid>
+				</form>
+			</div>
+		</Container>
+	);
 }
